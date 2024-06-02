cbcce87e6a968c7af134f2c80ce0cf8517f14695
forNerzul <sbeardman92@gmail.com>
2024-06-02 13:12:20 -0400

    feat(routing): Add login and register routes with PrivateRoute for home
    - Added LoginPage and RegisterPage components to routing.
    - Implemented PrivateRoute for the home path to restrict access.
    - Updated route structure to include paths for "/", "/login", and "/register".
    - Ensured consistent formatting and indentation for better readability.

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LoginPage, RegisterPage } from "./components/pages";
+import { PrivateRoute } from "./components/organisms";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Home</div>,
-  },
+    {
+        path: "/",
+        element: (
+            <PrivateRoute>
+                <h1>Hola</h1>
+            </PrivateRoute>
+        ),
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+    {
+        path: "/register",
+        element: <RegisterPage />,
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
